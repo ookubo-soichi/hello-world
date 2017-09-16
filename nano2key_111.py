7f76b898579c670e548da098ef118d5faec16cde
ookubo-soichi <ookubo@jsk.imi.i.u-tokyo.ac.jp>
2017-09-16 22:18:36 +0900

    Add files via upload

---
+++ nano2key_111.py
@@ -0,0 +1,304 @@
+# coding: UTF-8
+#!/usr/bin/env python
+import os, sys, time
+import pygame
+import pygame.midi
+from pygame.locals import *
+import pyautogui as pa
+
+cm_length = [1.0, 1.0, 1.0]
+cycle_tune = ['+', '-', '-']
+
+this_ops=True   # OP Skip
+this_eds=True   # ED Skip
+this_lcms=True  # Last CM Skip
+fs_flag = False # Full Screen at Next Chapter
+
+cycle_index = 0
+
+def set_cycle_time(cml=[1.0, 1.0, 1.0], ops=True, eds=True, lcms=True):
+    # CM Length (min)
+    result = cml
+    # OP Skip
+    if ops:
+        result[0] = result[0]+1.5
+    # ED and the Following CM Skip
+    if eds and lcms:
+        result[-1] = result[-1]+1.5
+    elif eds and (not lcms):
+        result[-1] = 1.5
+    elif (not eds) and lcms:
+        pass
+    else:
+        result[-1] = 0.0
+    return result
+
+cycle_time = set_cycle_time(cml=cm_length, ops=this_ops, eds=this_eds, lcms=this_lcms)
+
+# display a list of MIDI devices connected to the computer
+def print_device_info():
+    for i in range( pygame.midi.get_count() ):
+        r = pygame.midi.get_device_info(i)
+        (interf, name, input, output, opened) = r
+        in_out = ""
+        if input:
+            in_out = "(input)"
+        if output:
+            in_out = "(output)"
+        print ("%2i: interface: %s, name: %s, opened: %s %s" %
+               (i, interf, name, opened, in_out))
+
+pygame.init()
+pygame.fastevent.init()
+event_get = pygame.fastevent.get
+event_post = pygame.fastevent.post
+pygame.midi.init()
+
+print ("Available MIDI devices:")
+print_device_info();
+
+# Change this to override use of default input device
+device_id = None
+if device_id is None:
+    input_id = pygame.midi.get_default_input_id()
+else:
+    input_id = device_id
+print ("Using input_id: %s" % input_id)
+i = pygame.midi.Input( input_id )
+
+print ("Logging started:")
+going = True
+
+isSet = False
+ch1 = [0,0,0,0,0,0,0,0]
+ch1_bin = [0,0,0,0,0,0,0,0]
+
+isFrame = False
+
+def one_min_after():
+    pa.hotkey('ctrl', 'right')
+def ten_sec_after():
+    pa.hotkey('alt', 'right')
+def one_min_before():
+    pa.hotkey('ctrl', 'left')
+def ten_sec_before():
+    pa.hotkey('alt', 'left')
+
+def skip_time(x):
+    min = int(x)
+    sec = int(6*(x-min))
+    for i in range(min):
+        one_min_after()
+    for i in range(sec):
+        ten_sec_after()
+def skip_tune(x):
+    for i in range(x.count('-')):
+        pa.press('pagedown')
+    for i in range(x.count('+')):
+        pa.press('pageup')
+
+def cycle_skip():
+    global cycle_index
+    skip_time(cycle_time[cycle_index])
+    skip_tune(cycle_tune[cycle_index])
+    cycle_index = cycle_index+1
+    if cycle_index >= len(cycle_time):
+        cycle_index = 0
+def cycle_reset():
+    global cycle_index
+    cycle_index = 0
+
+def op_ed_skip():
+    skip_time(1.5)
+    skip_tune('-')
+
+while going:
+    events = event_get()
+    if isFrame and len(events)==0:
+        pa.press('e')
+        time.sleep(1)
+    for e in events:
+        if e.type in [QUIT]:
+            going = False
+        if e.type in [KEYDOWN]:
+            going = False
+        if e.type in [pygame.midi.MIDIIN]:
+            # print information to console
+            print ("Timestamp: " + str(e.timestamp) + "ms, Channel: " + str(e.data1) + ", Value: " + str(e.data2)+", set "+str(isSet)+", ci "+str(cycle_index)+str(isFrame))
+
+            # Hot-Key
+            # Start / Pause
+            if e.data1 == 41 and e.data2 == 127:
+                pa.press(' ')
+            # Full Screen
+            if e.data1 == 42 and e.data2 == 127:
+                pa.press('f')
+            # Variable Skip
+            for ich in range(len(ch1)):
+                if e.data1 == ich:
+                    ch1[ich] = e.data2
+                    if ch1[ich] >= 60:
+                        ch1_bin[ich] = 1
+                    else:
+                        ch1_bin[ich] = 0
+            # Cycle Skip
+            if e.data1 == 45 and e.data2 == 127:
+                if isSet:
+                    op_ed_skip()
+                else:
+                    cycle_skip()
+                    
+            # Next
+            if e.data1 == 44 and e.data2 == 127:
+                pa.press('n')
+                if fs_flag:
+                    pa.press('f')
+                skip_tune('+'*sum(ch1_bin))
+                cycle_reset()
+            # Previous
+            if e.data1 == 43 and e.data2 == 127:
+                pa.press('p')
+                cycle_reset()
+                
+            # isSet
+            if e.data1 == 60:
+                if e.data2 == 127:
+                    isSet = True
+                else:
+                    isSet = False 
+            # Faster
+            if e.data1 == 62 and e.data2 == 127:
+                if isSet:
+                    pa.press(']')
+                else:
+                    pa.press(';')
+            # Slower
+            if e.data1 == 61 and e.data2 == 127:
+                if isSet:
+                    pa.press('[')
+                else:
+                    pa.press('-')
+                    
+            # Volume Up
+            if e.data1 == 59 and e.data2 == 127:
+                pa.hotkey('ctrl', 'up')
+            # Volume Down
+            if e.data1 == 58 and e.data2 == 127:
+                pa.hotkey('ctrl', 'down')
+            # Cycle Reset
+            if e.data1 == 46 and e.data2 == 127:
+                cycle_reset()
+
+            # Skip Time Setting
+            # 3 sec before
+            if e.data1 == 64 and e.data2 == 127:
+                pa.press('pagedown')
+            # 3 sec after
+            if e.data1 == 65 and e.data2 == 127:
+                pa.press('pageup')
+            # 10 sec before
+            if e.data1 == 48 and e.data2 == 127:
+                ten_sec_before()
+            # 10 sec after
+            if e.data1 == 49 and e.data2 == 127:
+                ten_sec_after()
+            # 1 min before
+            if e.data1 == 32 and e.data2 == 127:
+                one_min_before()
+            # 1 min after
+            if e.data1 == 33 and e.data2 == 127:
+                one_min_after()
+            # 27 sec after
+            if e.data1 == 66 and e.data2 == 127:
+                ten_sec_after()
+                ten_sec_after()
+                ten_sec_after()
+                pa.press('pagedown')
+            # 57 sec after
+            if e.data1 == 50 and e.data2 == 127:
+                one_min_after()
+                pa.press('pagedown')
+            # 1 min 27sec after
+            if e.data1 == 34 and e.data2 == 127:
+                one_min_after()
+                ten_sec_after()
+                ten_sec_after()
+                ten_sec_after()
+                pa.press('pagedown')
+            # 1 min 57 sec after
+            if e.data1 == 67 and e.data2 == 127:
+                one_min_after()
+                one_min_after()
+                pa.press('pagedown')
+            # 2 min 27 sec after
+            if e.data1 == 51 and e.data2 == 127:
+                one_min_after()
+                one_min_after()
+                ten_sec_after()
+                ten_sec_after()
+                ten_sec_after()
+                pa.press('pagedown')
+            # 2 min 57 sec after
+            if e.data1 == 35 and e.data2 == 127:
+                one_min_after()
+                one_min_after()
+                one_min_after()
+                pa.press('pagedown')
+                
+            # Speed Settings
+            # x1.0
+            if e.data1 == 70 and e.data2 == 127:
+                pa.press('u')
+            # x1.5
+            if e.data1 == 54 and e.data2 == 127:
+                pa.press('u')
+                pa.press(';')
+            # x2.1
+            if e.data1 == 38 and e.data2 == 127:
+                pa.press('u')
+                pa.press(';')
+                pa.press(';')
+                pa.press(']')
+            # x2.3
+            if e.data1 == 71 and e.data2 == 127:
+                pa.press('u')
+                pa.press(';')
+                pa.press(';')
+                pa.press(']')
+                pa.press(']')
+                pa.press(']')
+            # x2.5
+            if e.data1 == 55 and e.data2 == 127:
+                pa.press('u')
+                pa.press(';')
+                pa.press(';')
+                pa.press(']')
+                pa.press(']')
+                pa.press(']')
+                pa.press(']')
+                pa.press(']')
+            # x2.8
+            if e.data1 == 39 and e.data2 == 127:
+                pa.press('u')
+                pa.press(';')
+                pa.press(';')
+                pa.press(';')
+                pa.press('[')
+                pa.press('[')
+
+            # Next Frame
+            if e.data1 == 69 and e.data2 == 127:
+                pa.press('e')
+            # Frame by Frame
+            if e.data1 == 53:
+                if e.data2 == 127:
+                    isFrame = True
+                else:
+                    isFrame = False
+
+    # if there are new data from the MIDI controller
+    if i.poll():
+        midi_events = i.read(10)
+        midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
+        for m_e in midi_evs:
+            event_post( m_e )
